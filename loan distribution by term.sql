57084cea48d25b3e4077c695bb19da4c4ee89409
data-girlCode <137500157+data-girlCode@users.noreply.github.com>
2023-10-08 02:27:25 +0100

    Update loan distribution by term.sql

--- loan distribution by term.sql
+++ loan distribution by term.sql
@@ -3,3 +3,26 @@ SELECT term,COUNT(*) AS loan_distribution
 FROM raqib
 GROUP BY 1
 ORDER BY 1 Asc
+
+-- Average Borrower APR by Employment Status
+SELECT employmentstatus, AVG(borrowerapr) AS avg_borrower_apr
+FROM raqib 
+GROUP BY 1
+
+-- Monthly Income Distribution for Homeowners vs. Non-Homeowners
+SELECT isborrowerhomeowner, statedmonthlyincome, COUNT(*) countofborrowers
+FROM raqib
+GROUP BY 1,2
+
+-- Loan with highest original amount
+SELECT loanoriginalamount
+FROM raqib
+WHERE loanoriginalamount = (SELECT MAX(loanoriginalamount)
+							FROM raqib)
+
+--Top 5 occupation by average stated monthly income
+SELECT occupation, AVG(statedmonthlyincome) AS Avgmonthlyincome
+FROM raqib
+GROUP BY 1
+ORDER BY Avgmonthlyincome DESC
+LIMIT 5
